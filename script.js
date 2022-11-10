ecfbf402042ee0d94d2b167b12bb9b3edfa9ac78
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-10 19:07:49 +0100

    Aggiunti input da layout allo script

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-/*let distance = prompt("Quanti chilometri intende percorrere?");
+function Price() {
+    let distance = document.getElementById("formDistanceInput").value;
 
-// --- Questo viene fatto nel caso in cui l'utente imetta i chilometri con la virgola --- //
-let right_distance = distance.replace(",", ".");
+    // --- Questo viene fatto nel caso in cui l'utente imetta i chilometri con la virgola --- //
+    let right_distance = distance.replace(",", ".");
 
-let distanceNaN = isNaN(right_distance);
+    let distanceNaN = isNaN(right_distance);
 
-if (distanceNaN || right_distance <= 0) {
-    console.error("Per favore, imettere un numero valido di chilometri.")
-};
+    if (distanceNaN || right_distance <= 0) {
+        console.error("Per favore, imettere un numero valido di chilometri.")
+    };
 
-let age = prompt("Indichi la sua età");
-let ageNaN = isNaN(age);
+    let fare = document.getElementById("FareSelect");
+    let value_fare = fare.value
 
-if (ageNaN || age <= 0) {
-    console.error("Per favore, imettere un'età vailda.")
-};
+    console.log(value_fare)
 
-let first_price = right_distance * 0.21;
+    let first_price = right_distance * 0.21;
 
-if (age < 18) {
-    var final_price = first_price - 0.2 * first_price;
-} else if (age >= 65) {
-    var final_price = first_price - 0.4 * first_price;
-} else {
-    var final_price = first_price;
-};
+    if (value_fare == 2) {
+        var final_price = first_price - 0.2 * first_price;
+    } else if (value_fare == 3) {
+        var final_price = first_price - 0.4 * first_price;
+    } else {
+        var final_price = first_price;
+    };
 
-let final_price_decimal = final_price.toFixed(2);
+    let final_price_decimal = final_price.toFixed(2);
 
-console.log(
-    `
+    document.getElementById("ticket_price").innerHTML = "PREZZO:" + final_price_decimal + "€";
+
+    let random_number = Math.floor((Math.random() * 99999999) + 10000000);
+
+    document.getElementById("ticket_number").innerHTML = random_number;
+
+    console.log(
+        `
     ------------------------------------Prezzo finale del biglieto-------------------------------------
 
     Il prezzo finale del vostro biglieto (applcando eventuali sconti) risulta: ${final_price_decimal} € 
@@ -37,6 +42,5 @@ console.log(
     ---------------------------------------------------------------------------------------------------
 
     `
-);
-
-*/
+    );
+}
